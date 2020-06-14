761c23e1d3a4d5854387a8770538dad6dcae6ab5
NguyenvanLuc06081994 <nvluc0608@gmail.com>
2020-06-14 09:39:59 +0700

    add addPlayer Function MainJs

--- JavaScriptFile/classMain.js
+++ JavaScriptFile/classMain.js
@@ -1,6 +1,21 @@
 let FootballClub = new FootballClub("U23 Viet Nam");
 FootballClub.getInit();
+
 function display(table) {
     document.getElementById('display').innerHTML = table;
 }
+
 display(FootballClub.getHtmlFC());
+
+function addPlayer() {
+    let img = document.getElementById('player-img').value;
+    let name = document.getElementById('player-name').value;
+    let age = document.getElementById('player-age').value;
+    let position = document.getElementById('player-position').value;
+    let country = document.getElementById('player-country').value;
+    let transferPrice = document.getElementById('player-transferPrice').value;
+    let player = new Player(img, name, age, position, country, transferPrice);
+    FootballClub.AddPlayer(player);
+    display(FootballClub.getHtmlFC());
+    document.getElementById('form-add').reset();
+}
